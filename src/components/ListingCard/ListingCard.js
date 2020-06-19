567562c4bdbb6cdf3c112d5d7627d15f6d614f98
Ashes <ashes.naskar@bitcanny.com>
2020-06-19 17:58:02 +0530

    Added social icon

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -10,7 +10,7 @@ import { formatMoney } from '../../util/currency';
 import { richText } from '../../util/richText';
 import { createSlug } from '../../util/urlHelpers';
 import config from '../../config';
-import { NamedLink, ResponsiveImage, Button } from '../../components';
+import { NamedLink, ResponsiveImage, Button, ExternalLink } from '../../components';
 import { updateProfile } from '../../containers/ProfileSettingsPage/ProfileSettingsPage.duck';
 import { showUser } from '../../containers/ProfilePage/ProfilePage.duck';
 import { showListing } from '../../containers/ListingPage/ListingPage.duck';
@@ -325,21 +325,23 @@ export class ListingCardComponent extends Component {
               )}
             </div>
           ) : null}
-          <a href={linkedinLink} className={css.socialLink}>
+          <ExternalLink href={linkedinLink} className={css.socialLink}>
+            {/* <a href={linkedinLink} className={css.socialLink}> */}
             {/* Linked-in Link */}
             {/*linkedinLink*/}
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              xmlns="http://www.w3.org/1999/xlink"
-              width="16"
-              height="17"
+              // xmlns="http://www.w3.org/1999/xlink"
+              width="22"
+              height="27"
             >
               <path
                 d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"
                 fill="#626262"
               />
             </svg>
-          </a>
+            {/* </a> */}
+          </ExternalLink>
         </div>
       </div>
     );
--- src/containers/ListingPage/ListingPage.js
+++ src/containers/ListingPage/ListingPage.js
@@ -40,6 +40,7 @@ import {
   LayoutWrapperFooter,
   Footer,
   BookingPanel,
+  ExternalLink,
 } from '../../components';
 import { EnquiryForm } from '../../forms';
 import { TopbarContainer, NotFoundPage } from '../../containers';
@@ -513,14 +514,32 @@ export class ListingPageComponent extends Component {
                   </div>
 
                   <span>
-                    <a
+                    {/* <a
                       href={linkedinLink}
                       target="_blank"
                       title={linkedinLinkMsg}
                       className={css.sociallink}
                     >
                       Linked-in Link
-                    </a>
+                    </a> */}
+                    <ExternalLink
+                      href={linkedinLink}
+                      target="_blank"
+                      title={linkedinLinkMsg}
+                      className={css.socialLinkedInlink}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        // xmlns="http://www.w3.org/1999/xlink"
+                        width="22"
+                        height="27"
+                      >
+                        <path
+                          d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"
+                          fill="#626262"
+                        />
+                      </svg>
+                    </ExternalLink>
                   </span>
                   <span>
                     <a
