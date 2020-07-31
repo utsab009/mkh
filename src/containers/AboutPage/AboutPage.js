ceffbb68cac0e5d1671b6a29a91beddbb0023128
developer-surajit <developer.surajit@gmail.com>
2020-07-31 12:44:27 +0530

    about static complete

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -35,6 +35,7 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
+          <h1 className={css.pageTitle}>About Try A Mentor</h1>
           <h1 className={css.pageTitle}>Their Experience, Your Success …</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
@@ -49,36 +50,17 @@ const AboutPage = () => {
               </h3>
 
               <p>
-              Tryamentor.com is a place and means for people who have a successful track record performing a
-              role to mentor people new to these roles or who hope to enter them. They can equally help a person
-              already in a role to further excel in it.
+              Tryamentor.com is a website and mobile phone application that enables people who have a successful track record performing ajob / career role to find and thenMentor people new to these roles or who hope to enter them. Due to this experience they can equally help a person already in a role to further excel in it. So, if you have such a track record, Tryamentor.com will enable you to both share and monetize this experience while simultaneously empowering others to directly gain from it.
               </p>
 
-              <p>Tryamentor.com empowers people to both share and monetize their experience while simultaneously
-              empowering others to directly gain from it – hence our tagline – “Their Experience, Your Success”.</p>
-              <p>So, if you are planning your next career step, simply search the site to find a wonderful Mentor who
-              can credibly guide you on how to get and succeed in your desired Job Role. Or, if you are currently in
-              a role and would like independent and highly effective support – you have found the right place.</p>
+              <p>Equally it is a place where you can find a Mentor. If you are seeking someone who has direct experience in a Job Role you wish to have or are already in, then you will find a range of different Mentors, with unique and common strengths and experience to choose from.</p>
+              <p>So, if you are planning your next career step, simply search the site to find a wonderful Mentor who can credibly guide you on how to get and succeed in your desired Job Role. Or, if you are currently in a role and would like independent and highly effective support – you have found the right place.</p>
 
-              <h3 className={css.subtitle}>Become A Mentor</h3>
+              <h3 className={css.subtitle}>Who we are</h3>
 
-              <p>
-              Once you have performed a role, you have experience. What Try A Mentor empowers you to do, is
-              find people (Mentees) who could directly gain from this experience. It is simply a question of you
-              understanding that this experience is valuable and signing up to be a Mentor on Tryamentor.com. You
-              place the details required for people to want to be mentored by you and on tryamentor.com you will
-              find lots of supports to help you build the skills of a great Mentor.
-              </p>
+              <p>Try A Mentor was started by Kenneth D. Glynn (founder). It is now a part of his overarching company, the Beacon HRM Group which is in turn an international Talent Consultancy. Beacon supplies Training, Executive Coaching, eLearning, and Talent Management services across the globe through its divisions of Beacon Training, Beacon Talent and eBeacon. Offices are in New York, San Francisco, and Dublin. Visit our parent website by clicking this link <ExternalLink href="http://www.beaconhrm.com">www.beaconhrm.com</ExternalLink> </p>
 
-              <h3 id="contact" className={css.subtitle}>
-              Who we are
-              </h3>
-              <p>
-              Try A Mentor was started by Kenneth D. Glynn (founder). It is now a part of his overarching company,
-              the Beacon HRM Group which is in turn an international Talent Consultancy. Beacon supplies Training, Executive Coaching, eLearning, and Talent Management services across the globe through
-              its divisions of Beacon Training, Beacon Talent and eBeacon. Visit our parent website by clicking this
-              link.
-              </p>
+              
               {/* <p>
                 You can also checkout our{' '}
                 <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
